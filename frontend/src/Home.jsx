67bf4f5f37a3ee98fc226f991d64006295d4dcf4
unknown <laz51@pitt.edu>
2023-04-08 19:28:21 -0400

    Example list page

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+const Home = () => {
+    return (
+        <div className="bg-gray-900">
+            <Header />
+        </div>
+    );
+};
+
+export default Home;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Home from "./Home";
+import Spending from "./components/Spending";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+    QueryClient,
+    QueryClientProvider,
+} from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//--------------Providers----------------
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/spending",
+        element: <Spending />,
+    },
+]);
+
+const queryClient = new QueryClient();
+
+//-----------------App-----------------
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    </React.StrictMode>
+);
