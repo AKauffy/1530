2cac8c75531f4da857893965ec1da552f77d49d3
unknown <laz51@pitt.edu>
2023-04-17 18:34:32 -0400

    Fixed

--- frontend/src/components/Budget.jsx
+++ frontend/src/components/Budget.jsx
@@ -29,6 +29,45 @@ const Budget = () => {
             }
         );
     };
+    const onChangeInput = (e, id) => {
+        const { Amount, value } = e.target;
+        console.log(e.target);
+        console.log(Amount);
+        console.log("value", value);
+        console.log("ID", id);
+
+        const editData = budget.map((item) => {
+            if (item._id == id) {
+                item.Amount = value;
+                return item;
+            }
+            return item;
+        });
+
+        setBudget(editData);
+    };
+
+    const editBudget = (budget) => {
+        console.log("BUDGET", budget);
+        const request = {
+            _id: budget._id,
+            title: budget.title,
+            Amount: budget.Amount,
+            user_id: budget.user_id,
+        };
+        console.log("REQUEST", request);
+        fetch(`/api/api/budget/${budget._id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(request),
+        }).then((response) => {
+            response = response.json().then((data) => {
+                console.log(data);
+            });
+        });
+    };
 
     useEffect(() => {
         getBudget();
@@ -91,10 +130,36 @@ const Budget = () => {
                                             <td className="py-4 pl-4 pr-3 text-sm sm:pl-6">
                                                 {bud.title}
                                             </td>
-                                            <td className="px-3 py-4 text-sm text-gray-500 lg:table-cell">
-                                                {bud.Amount}
+                                            <td className="px-3 py-4 text-sm text-gray-500">
+                                                <input
+                                                    name="Amount"
+                                                    value={bud.Amount}
+                                                    onChange={(e) =>
+                                                        onChangeInput(
+                                                            e,
+                                                            bud._id
+                                                        )
+                                                    }
+                                                ></input>
                                             </td>
-                                            <td className=" py-4 text-sm text-gray-500 lg:table-cell">
+                                            <td className=" py-4 text-sm text-gray-500 lg:table-cell flex flex-row">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth={1.5}
+                                                    stroke="currentColor"
+                                                    className="w-6 h-6 rounded-lg cursor-pointer"
+                                                    onClick={() => {
+                                                        editBudget(bud);
+                                                    }}
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                                                    />
+                                                </svg>
                                                 <svg
                                                     xmlns="http://www.w3.org/2000/svg"
                                                     fill="none"
